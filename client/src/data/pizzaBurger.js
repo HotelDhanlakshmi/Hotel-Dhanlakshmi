57626d84399536357ac64b2cb629b449442e2235
Mahesh Shinde <maheshshinde9100@gmail.com>
2025-11-02 23:43:13 +0530

    pizza burgers

--- client/src/data/pizzaBurger.js
+++ client/src/data/pizzaBurger.js
@@ -1,10 +1,10 @@
 export const pizzaBurgerItems = [
-  { id: 'pb1', name: 'Veg Cheese Pizza', price: 169, category: 'pizza-burger', type: 'veg', image: '/images/veg-cheese-pizza.jpg' },
-  { id: 'pb2', name: 'Cheese Burst Pizza', price: 199, category: 'pizza-burger', type: 'veg', image: '/images/cheese-burst-pizza.jpg' },
-  { id: 'pb3', name: 'Margarita Pizza', price: 199, category: 'pizza-burger', type: 'veg', image: '/images/margarita-pizza.jpg' },
-  { id: 'pb4', name: 'Paneer Tikka Pizza', price: 199, category: 'pizza-burger', type: 'veg', image: '/images/paneer-tikka-pizza.jpg' },
-  { id: 'pb5', name: 'Mushroom Pizza', price: 199, category: 'pizza-burger', type: 'veg', image: '/images/mushroom-pizza.jpg' },
-  { id: 'pb6', name: 'Cheese Pizza Puff', price: 99, category: 'pizza-burger', type: 'veg', image: '/images/cheese-pizza-puff.jpg' },
-  { id: 'pb7', name: 'Schezwan Pizza', price: 149, category: 'pizza-burger', type: 'veg', image: '/images/schezwan-pizza.jpg' },
-  { id: 'pb8', name: 'Classic Onion', price: 149, category: 'pizza-burger', type: 'veg', image: '/images/classic-onion.jpg' }
+  { id: 'pb1', name: 'Veg Cheese Pizza', price: 169, category: 'pizza-burger', type: 'veg', image: 'https://images.unsplash.com/photo-1565299624946-b28f40a0ca4b?w=400&h=300&fit=crop' },
+  { id: 'pb2', name: 'Cheese Burst Pizza', price: 199, category: 'pizza-burger', type: 'veg', image: 'https://images.unsplash.com/photo-1513104890138-7c749659a591?w=400&h=300&fit=crop' },
+  { id: 'pb3', name: 'Margarita Pizza', price: 199, category: 'pizza-burger', type: 'veg', image: 'https://images.unsplash.com/photo-1604382354936-07c5d9983bd3?w=400&h=300&fit=crop' },
+  { id: 'pb4', name: 'Paneer Tikka Pizza', price: 199, category: 'pizza-burger', type: 'veg', image: 'https://images.unsplash.com/photo-1571407970349-bc81e7e96d47?w=400&h=300&fit=crop' },
+  { id: 'pb5', name: 'Mushroom Pizza', price: 199, category: 'pizza-burger', type: 'veg', image: 'https://images.unsplash.com/photo-1585238342024-78d387f4a707?w=400&h=300&fit=crop' },
+  { id: 'pb6', name: 'Cheese Pizza Puff', price: 99, category: 'pizza-burger', type: 'veg', image: 'https://images.unsplash.com/photo-1534308983496-4fabb1a015ee?w=400&h=300&fit=crop' },
+  { id: 'pb7', name: 'Schezwan Pizza', price: 149, category: 'pizza-burger', type: 'veg', image: 'https://images.unsplash.com/photo-1628840042765-356cda07504e?w=400&h=300&fit=crop' },
+  { id: 'pb8', name: 'Classic Onion', price: 149, category: 'pizza-burger', type: 'veg', image: 'https://images.unsplash.com/photo-1506354666786-959d6d497f1a?w=400&h=300&fit=crop' }
 ];
