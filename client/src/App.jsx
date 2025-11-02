87a7ddd571a7f87475c4e6423b789306b1a4e289
Mahesh Shinde <maheshshinde9100@gmail.com>
2025-11-02 23:02:47 +0530

    App.jsx commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AppProvider } from './context/AppContext';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+import OrderTracking from './pages/OrderTracking';
+import AuthModal from './components/AuthModal';
+
 function App() {
-  return(
-    <></>
-  )
+  return (
+    <AppProvider>
+      <Router>
+        <div className="min-h-screen bg-gray-50">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/track-order/:orderId" element={<OrderTracking />} />
+          </Routes>
+          <AuthModal />
+        </div>
+      </Router>
+    </AppProvider>
+  );
 }
 
-export default App
+export default App;
