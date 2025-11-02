d200645f26a7bf05ba615ad66740f2f62b5e4211
Mahesh Shinde <maheshshinde9100@gmail.com>
2025-11-02 23:42:50 +0530

    .

--- client/src/data/mutton.js
+++ client/src/data/mutton.js
@@ -1,14 +1,14 @@
 export const muttonItems = [
-  { id: 'mt1', name: 'Mutton Fry', price: 250, category: 'mutton', type: 'non-veg', image: '/images/mutton-fry.jpg' },
-  { id: 'mt2', name: 'Mutton Roast', price: 250, category: 'mutton', type: 'non-veg', image: '/images/mutton-roast.jpg' },
-  { id: 'mt3', name: 'Mutton Kadhai', price: 379, category: 'mutton', type: 'non-veg', image: '/images/mutton-kadhai.jpg' },
-  { id: 'mt4', name: 'Mutton Mughlai', price: 379, category: 'mutton', type: 'non-veg', image: '/images/mutton-mughlai.jpg' },
-  { id: 'mt5', name: 'Mutton Curry', price: 260, category: 'mutton', type: 'non-veg', image: '/images/mutton-curry.jpg' },
-  { id: 'mt6', name: 'Mutton Dahiwala', price: 320, category: 'mutton', type: 'non-veg', image: '/images/mutton-dahiwala.jpg' },
-  { id: 'mt7', name: 'Mutton Bhuna', price: 330, category: 'mutton', type: 'non-veg', image: '/images/mutton-bhuna.jpg' },
-  { id: 'mt8', name: 'Mutton Do Pyaaz', price: 360, category: 'mutton', type: 'non-veg', image: '/images/mutton-do-pyaaz.jpg' },
-  { id: 'mt9', name: 'Mutton Keema', price: 350, category: 'mutton', type: 'non-veg', image: '/images/mutton-keema.jpg' },
-  { id: 'mt10', name: 'Mutton Malvani Half', price: 549, category: 'mutton', type: 'non-veg', image: '/images/mutton-malvani-half.jpg' },
-  { id: 'mt11', name: 'Mutton Tikka Masala', price: 320, category: 'mutton', type: 'non-veg', image: '/images/mutton-tikka-masala.jpg' },
-  { id: 'mt12', name: 'Mutton Handi Half', price: 450, category: 'mutton', type: 'non-veg', image: '/images/mutton-handi-half.jpg' }
+  { id: 'mt1', name: 'Mutton Fry', price: 250, category: 'mutton', type: 'non-veg', image: 'https://images.unsplash.com/photo-1544025162-d76694265947?w=400&h=300&fit=crop' },
+  { id: 'mt2', name: 'Mutton Roast', price: 250, category: 'mutton', type: 'non-veg', image: 'https://images.unsplash.com/photo-1529692236671-f1f6cf9683ba?w=400&h=300&fit=crop' },
+  { id: 'mt3', name: 'Mutton Kadhai', price: 379, category: 'mutton', type: 'non-veg', image: 'https://images.unsplash.com/photo-1585937421612-70a008356fbe?w=400&h=300&fit=crop' },
+  { id: 'mt4', name: 'Mutton Mughlai', price: 379, category: 'mutton', type: 'non-veg', image: 'https://images.unsplash.com/photo-1603133872878-684f208fb84b?w=400&h=300&fit=crop' },
+  { id: 'mt5', name: 'Mutton Curry', price: 260, category: 'mutton', type: 'non-veg', image: 'https://images.unsplash.com/photo-1574484284002-952d92456975?w=400&h=300&fit=crop' },
+  { id: 'mt6', name: 'Mutton Dahiwala', price: 320, category: 'mutton', type: 'non-veg', image: 'https://images.unsplash.com/photo-1567188040759-fb8a883dc6d8?w=400&h=300&fit=crop' },
+  { id: 'mt7', name: 'Mutton Bhuna', price: 330, category: 'mutton', type: 'non-veg', image: 'https://images.unsplash.com/photo-1631452180519-c014fe946bc7?w=400&h=300&fit=crop' },
+  { id: 'mt8', name: 'Mutton Do Pyaaz', price: 360, category: 'mutton', type: 'non-veg', image: 'https://images.unsplash.com/photo-1606491956689-2ea866880c84?w=400&h=300&fit=crop' },
+  { id: 'mt9', name: 'Mutton Keema', price: 350, category: 'mutton', type: 'non-veg', image: 'https://images.unsplash.com/photo-1610057099431-d73a1c9d2f2f?w=400&h=300&fit=crop' },
+  { id: 'mt10', name: 'Mutton Malvani Half', price: 549, category: 'mutton', type: 'non-veg', image: 'https://images.unsplash.com/photo-1599487488170-d11ec9c172f0?w=400&h=300&fit=crop' },
+  { id: 'mt11', name: 'Mutton Tikka Masala', price: 320, category: 'mutton', type: 'non-veg', image: 'https://images.unsplash.com/photo-1565557623262-b51c2513a641?w=400&h=300&fit=crop' },
+  { id: 'mt12', name: 'Mutton Handi Half', price: 450, category: 'mutton', type: 'non-veg', image: 'https://images.unsplash.com/photo-1588166524941-3bf61a9c41db?w=400&h=300&fit=crop' }
 ];
